5e431e00c6475cf2c9147ccfa1470d51a9714049
Yoshifumi-T <2g.4423@gmail.com>
2020-05-08 17:36:19 +0900

    リサイズに対応

--- js/background.js
+++ js/background.js
@@ -1,21 +1,23 @@
 'usestrict'
 
-//描画コンテキストの取得
-const body   = document.querySelector('body');
-const canvas = document.querySelector('#js-canvas');
-const width  = body.clientWidth;
-const height = body.clientHeight;
-canvas.setAttribute('width',`${width}`);
-canvas.setAttribute('height', `${height}px`);
+function renderBackground() {
+    //描画コンテキストの取得
+    const body   = document.querySelector('body');
+    const canvas = document.querySelector('#js-canvas');
+    const width  = body.clientWidth;
+    const height = body.clientHeight;
+    canvas.setAttribute('width',`${width}`);
+    canvas.setAttribute('height', `${height}px`);
 
-if (canvas.getContext) {
-    const context = canvas.getContext('2d');
+    if (canvas.getContext) {
+        const context = canvas.getContext('2d');
 
-    context.fillStyle = '#FFFFFF';
-    for(let i = 0; i < 1000; i++) {
-        const x = getRandomInt(0, width);
-        const y = getRandomInt(0, height);
-        context.fillRect(x, y, 1, 1);
+        context.fillStyle = '#FFFFFF';
+        for(let i = 0; i < 1000; i++) {
+            const x = getRandomInt(0, width);
+            const y = getRandomInt(0, height);
+            context.fillRect(x, y, 1, 1);
+        }
     }
 }
 
@@ -24,3 +26,6 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
+
+renderBackground();
+window.onresize = renderBackground;
